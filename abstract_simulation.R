f583048b7f0f9895d65a0a7552e2b3b653b896b7
michbur <michalburdukiewicz@gmail.com>
2015-01-27 22:30:27 +0100

    finished simulation code

--- abstract_simulation.R
+++ abstract_simulation.R
@@ -1,22 +1,36 @@
 library(dpcR)
 library(pbapply)
-sim_dat <- pblapply(1L:15*10, function(m1) {
-  res <- lapply(1L:15*10, function(m2) {   
-    adpcr1 <- sim_adpcr(m = m1, n = 765, times = 1e6, pos_sums = FALSE, n_panels = 3)
-    adpcr2 <- sim_adpcr(m = m2, n = 765, times = 1e6, pos_sums = FALSE, n_panels = 3)
-    
-    adpcrs <- bind_dpcr(adpcr1, adpcr2)
-    
-    compb <- test_counts(adpcrs, "binomial")
-    compr <- test_counts(adpcrs, "ratio")
-    compp <- test_counts(adpcrs, "prop")
-    
-    lapply(list(glm = compb, 
-                prop = compp, 
-                ratio = compr), function(single_test) as.numeric(coef(single_test)[["group"]]))
+
+sim_res <- pbsapply(1L:300, function(dummy) {
+  sim_dat2 <- lapply(1L:10*10, function(m1) {
+    m2_vector <- m1 + 1L:5*10
+    res <- lapply(m2_vector, function(m2) {   
+      adpcr1 <- sim_adpcr(m = m1, n = 765, times = 1e6, pos_sums = FALSE, n_panels = 3)
+      adpcr2 <- sim_adpcr(m = m2, n = 765, times = 1e6, pos_sums = FALSE, n_panels = 3)
+      
+      adpcrs <- bind_dpcr(adpcr1, adpcr2)
+      
+      compb <- test_counts(adpcrs, "binomial")
+      compr <- test_counts(adpcrs, "ratio")
+      compp <- test_counts(adpcrs, "prop")
+      
+      lapply(list(glm = compb, 
+                  prop = compp, 
+                  ratio = compr), function(single_test) as.numeric(coef(single_test)[["group"]]))
+    })
+    names(res) <- paste0("m2.", m2_vector)
+    res
   })
-  names(res) <- paste0("m2.", 1L:15*10)
-  res
+  
+  names(sim_dat2) <- paste0("m1.", 1L:10*10)
+  
+  c(mean((3 - sapply(sim_dat2, function(i)
+    sapply(i, function(j) {
+      sum(j[["glm"]][1L:3] != j[["glm"]][4L:6])
+    })))^2), mean((3 - sapply(sim_dat2, function(i)
+      sapply(i, function(j) {
+        sum(j[["ratio"]][1L:3] != j[["ratio"]][4L:6])
+      })))^2))
 })
 
-names(sim_dat) <- paste0("m1.", 1L:15*10)
+
