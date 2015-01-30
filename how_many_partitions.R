505c4ef25dce824c97364575703f3284cc635d0a
michbur <michalburdukiewicz@gmail.com>
2015-01-30 13:15:47 +0100

    simulations went ok.

--- how_many_partitions.R
+++ how_many_partitions.R
@@ -1,13 +1,13 @@
 library(dpcR)
 library(pbapply)
 
-dpcr_size = 20000L
+dpcr_size = 765
 
 dpcr_data <- function(n, pos)
   c(rep(1, pos), rep(0, n - pos))
 
-sim_res <- t(do.call(cbind, pblapply(1L:8000, function(pos1) 
-  sapply(1L:8000, function(pos2) {
+sim_res <- t(do.call(cbind, pblapply(1L:600, function(pos1) 
+  sapply(1L:600, function(pos2) {
     dpcr1 <- create_dpcr(dpcr_data(dpcr_size, pos1), dpcr_size, type = "np", adpcr = TRUE)
     dpcr2 <- create_dpcr(dpcr_data(dpcr_size, pos2), dpcr_size, type = "np", adpcr = TRUE)
     dpcrs <- bind_dpcr(dpcr1, dpcr2)
@@ -24,3 +24,7 @@ sim_res <- t(do.call(cbind, pblapply(1L:8000, function(pos1)
 
 colnames(sim_res) <- c("total", "pos1", "pos2", "pb", "pp", "pr")
 write.csv2(sim_res, file = "sim_res.csv")
+
+sim_res <- read.csv2("sim_res.csv")[, -1]
+library(ggplot2)
+ggplot(sim_res, aes(x = pos1, y = pos2, fill = pr)) + geom_tile()
