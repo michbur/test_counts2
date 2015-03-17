f9672bfebc7b668391be43bbb78b2b196631781b
michbur <michalburdukiewicz@gmail.com>
2015-03-17 20:17:18 +0100

    added plot

--- methods_framework.R
+++ methods_framework.R
@@ -15,16 +15,17 @@ get_ints <- function(number_of_exps) {
   list(ints = cbind(dpcrtab[seq(1, nrow(dpcrtab), by = 2), 3:5], 
                     dpcrtab[seq(1, nrow(dpcrtab), by = 2) + 1, 3:5],
                     glmtab[, -1],
-                    dpcrtab[, -1]),
+                    ratiotab[, -1]),
        real_m = colSums(dat),
        test_obj = test_counts(dat))
 }
 
 ints5 <- get_ints(5)
 
-colnames(ints5[["ints"]]) <- rep(c("lambda", "low", "up"), 3)
+
+colnames(ints5[["ints"]]) <- rep(c("lambda", "low", "up"), 4)
 method_names <- c("Dube", "Bhat", "GLM", "Ratio")
-indat <- do.call(rbind, lapply(0L:2, function(i) {
+indat <- do.call(rbind, lapply(0L:3, function(i) {
   data.frame(experiment = names(ints5[["real_m"]]),
              name = rep(method_names[i + 1], nrow(ints5[["ints"]])),
              real = ints5[["real_m"]],
