1e4175446631dd6667f52868a439f66a8f1b8d39
michbur <michalburdukiewicz@gmail.com>
2015-03-19 00:42:10 +0100

    still no idea how to add simultaneous CIs

--- prob_cov.R
+++ prob_cov.R
@@ -1,7 +1,7 @@
 library(dpcR)
 library(binom)
 
-sapply(0L:20*15, function(num_mol) {
+sapply(0L:20*20, function(num_mol) {
   number_of_exps <- 100
   adpcr1 <- sim_adpcr(m = num_mol, n = 1000, times = 100000, pos_sums = FALSE, 
                       n_panels = number_of_exps)
@@ -10,18 +10,18 @@ sapply(0L:20*15, function(num_mol) {
   positives <- colSums(dat > 0, na.rm = TRUE)
   total <- slot(dat, "n")
   
-  adj_ci <- binom.confint(positives, 
+  adj_ci <- dpcR:::fl(binom.confint(positives, 
                           total, 
                           conf.level = (1 - 0.05)^(1/ncol(dat)),
-                          "wilson")[, 4L:6][, 2L:3]
+                          "wilson")[, 4L:6])[, 2L:3]
   
-  nadj_ci <- binom.confint(positives, 
+  nadj_ci <- dpcR:::fl(binom.confint(positives, 
                            total, 
                            conf.level = 0.95,
-                           "wilson")[, 4L:6][, 2L:3]
-  
-  c(adj = sum(apply(cbind(positives/total, adj_ci), 1, function(i)
-    abs(i[1]) >= abs(i[2]) && i[1] <= i[3]))/number_of_exps,
-    nadj = sum(apply(cbind(positives/total, nadj_ci), 1, function(i)
-      i[1] >= i[2] && i[1] <= i[3]))/number_of_exps)
+                           "wilson")[, 4L:6])[, 2L:3]
+
+  c(adj = sum(apply(cbind(rep(num_mol, number_of_exps)/total, adj_ci), 1, function(i)
+              abs(i[1]) >= abs(i[2]) && i[1] <= i[3]))/number_of_exps,
+    nadj = sum(apply(cbind(rep(num_mol, number_of_exps)/total, nadj_ci), 1, function(i)
+               abs(i[1]) >= abs(i[2]) && i[1] <= i[3]))/number_of_exps)
 })
