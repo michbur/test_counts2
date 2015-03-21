377ca6f71c5548402a2a90c81f9c67ed7501cb20
michbur <michalburdukiewicz@gmail.com>
2015-03-21 08:58:33 +0100

    poster done

--- abstract_simulation.R
+++ abstract_simulation.R
@@ -6,7 +6,7 @@ library(reshape2)
 adpcr_comp <- pbsapply(1L:300, function(dummy) {
   sim_dat2 <- lapply(1L:10*10, function(m1) {
     m2_vector <- m1 + 1L:5*10
-    res <- lapply(m2_vector, function(m2) {   
+    res <- lapply(m2_vector, function(m2) { 
       adpcr1 <- sim_adpcr(m = m1, n = 765, times = 1e6, pos_sums = FALSE, n_panels = 3)
       adpcr2 <- sim_adpcr(m = m2, n = 765, times = 1e6, pos_sums = FALSE, n_panels = 3)
       
@@ -34,6 +34,6 @@ adpcr_comp <- pbsapply(1L:300, function(dummy) {
 
 
 rownames(adpcr_comp) <- c("GLM", "MT")
-madpcr_comp <- melt(sqrt(adpcr_comp)/6)
+madpcr_comp <- melt(sqrt(adpcr_comp))
 colnames(madpcr_comp) <- c("method", "repetition", "value")
-save(madpcr_comp, file = "adpcr_comp.RData")
+save(madpcr_comp, adpcr_comp, file = "adpcr_comp.RData")
